b7d261f160552a070c6394cee5479f6e27ffe3cf
Aleksey Lazarev <aleksey.lazarev@hotmail.com>
2023-08-22 12:39:01 +0300

    add problem #168

---
+++ CSharp.LeetCode/_168.cs
@@ -0,0 +1,17 @@
+namespace CSharp.LeetCode._168;
+
+//168. Excel Sheet Column Title
+//https://leetcode.com/problems/excel-sheet-column-title/description/
+public class Solution {
+    public string ConvertToTitle(int columnNumber) {
+        var result = "";
+        while(columnNumber!=0){
+
+            result = (char)('A'+(columnNumber-1)%26) + result;
+            columnNumber=(columnNumber-1)/26;
+
+        }
+
+        return result;
+    }
+}
